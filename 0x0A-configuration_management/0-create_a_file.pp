e1076275e98c83a28c8ee7a24a060e7d081a2f7f
Eric Godworks <ericgodworks@yahoo.com>
2023-06-28 05:05:44 -0700

    create puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 #Creates a file in /tmp
 
 file { '/tmp/school':
-  content =>'I love Puppet',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
